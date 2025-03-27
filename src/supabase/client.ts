f6b4858d9f8c63df53810d8c5dcb6c12e3ae1616
Alexander Smirnov <alex@asmirnov.xyz>
2025-03-27 18:42:17 -0500

    load envs in supabase

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -1,4 +1,7 @@
 import type { Database } from "../types/db"
 import { createClient } from "@supabase/supabase-js"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 export const supabase = createClient<Database>(process.env.SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!)
